821d958cc946fe00dd44c1873dfc12b33dbce02b
shubham <shubhamsarkar2587@gmail.com>
2022-12-25 03:40:43 +0530

    Title: Font family changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import JobDetail from "./pages/JobDetail";
 
 function App() {
   return (
-    <div className="app w-full font-sans overflow-hidden">
+    <div className="app w-full font-['Segoe_UI'] overflow-hidden">
       <JobDetail />
     </div>
   );
